975138f343709bf41d36f3006a1b3e950a2a02fe
Mukola-P <proxorov.mukola@gmail.com>
2022-01-28 08:45:57 +0200

    hw8

---
+++ src/js/01-gallery.js
@@ -0,0 +1,25 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+
+const galleryRef = document.querySelector('.gallery');
+
+function galleryMarkup(items) {
+  const markup = items
+    .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`;
+    })
+    .join('');
+
+  galleryRef.innerHTML = markup;
+}
+
+galleryMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
